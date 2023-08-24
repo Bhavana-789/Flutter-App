de92b9a704ba93de814afa431e9c6e7022ff5c8b
Bhavana-789 <100331843+Bhavana-789@users.noreply.github.com>
2023-08-24 11:16:02 +0530

    Added changes

---
+++ lib/pages/home_detail_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_flutter_app/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import '../models/catalog.dart';
+
+class HomeDetailPage extends StatelessWidget {
+  final Item catalog;
+  const HomeDetailPage({Key? key, required this.catalog})
+      : assert(catalog != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.transparent),
+      backgroundColor: MyTheme.creamColor,
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: ButtonBar(
+          alignment: MainAxisAlignment.spaceBetween,
+          // buttonPadding: Vx.mOnly(right: 20),
+          buttonPadding: EdgeInsets.zero,
+          children: [
+            "\$${catalog.price}".text.red700.xl4.bold.xl.make(),
+            ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(MyTheme.darkBlue),
+                        shape:
+                            MaterialStateProperty.all(const StadiumBorder())),
+                    onPressed: () {},
+                    child: "Add to cart".text.make())
+                .wh(120, 50)
+          ],
+        ).p32(),
+      ),
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Hero(tag: Key(catalog.image), child: Image.network(catalog.image))
+                .h32(context),
+            Expanded(
+                child: VxArc(
+              height: 30.0,
+              arcType: VxArcType.convey,
+              edge: VxEdge.top,
+              child: Container(
+                color: Colors.white,
+                width: context.screenWidth,
+                child: Column(
+                  children: [
+                    catalog.name.text.xl4.color(MyTheme.darkBlue).bold.make(),
+                    catalog.desc.text.xl.textStyle(context.captionStyle).make(),
+                    10.heightBox,
+                    "The services are provided \" As is \". We make no representation or warranty of any kind whatsoever to you or any other person relating in any way to the services, including any part thereof, or any website or other content or service that may be accessible directly or indirectly through the services "
+                        .text
+                        .textStyle(context.captionStyle)
+                        .make()
+                    .p16()
+                  ],
+                ).py64(),
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_flutter_app/pages/home_widgets/catalog_image.dart';
+import 'package:my_first_flutter_app/pages/home_detail_page.dart';
+import 'package:my_first_flutter_app/widgets/home_widgets/catalog_image.dart';
+
 import 'package:velocity_x/velocity_x.dart';
 import '../../models/catalog.dart';
-import '../../widgets/themes.dart';
-
+import '../themes.dart';
 
 class CatalogList extends StatelessWidget {
   @override
@@ -13,7 +14,12 @@ class CatalogList extends StatelessWidget {
         itemCount: CatalogModel.items.length,
         itemBuilder: (context, index) {
           final catalog = CatalogModel.items[index];
-          return CatalogItem(catalog: catalog);
+          return InkWell(
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => HomeDetailPage(catalog: catalog))),
+              child: CatalogItem(catalog: catalog));
         });
   }
 }
@@ -29,7 +35,9 @@ class CatalogItem extends StatelessWidget {
     return VxBox(
         child: Row(
       children: [
-        CatalogImage(image: catalog.image),
+        Hero(
+            tag: Key(catalog.image),
+            child: CatalogImage(image: catalog.image)),
         Expanded(
             child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -51,7 +59,7 @@ class CatalogItem extends StatelessWidget {
                         shape:
                             MaterialStateProperty.all(const StadiumBorder())),
                     onPressed: () {},
-                    child: "Buy".text.make())
+                    child: "Add to cart".text.make())
               ],
             ).pOnly(right: 8.0)
           ],
